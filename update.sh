ff288877cdeff3edb33a87c9fa9fafc6afe0ff34
tkas <tomas.kasparek@gmail.com>
2016-02-10 16:59:37 +0100

    osmupdate clip by polygon, cleanup config variables and comments

--- update.sh
+++ update.sh
@@ -1,21 +1,28 @@
 #!/bin/sh
-PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+#
 #Petr Svana, March 2015
 #
-#variables to set
-#
-#paths
-OSMOSIS=osmosis
-OSMFILTER=osmfilter
-OSMUPDATE=osmupdate
-OSMCONVERT=osmconvert
 
-#path to working directory
+#change your stuff here
+#--------------------------------------------------------
+#path to working directory where files will be stored
 WORK_DIR=/home/xsvana00/data
 
 #path to logfile
 LOGFILE=/home/xsvana00/update.log
-LASTUPDATE=/var/www/html/xsvana00/last_update.txt
+
+#path to web root - where to call PHP scripts and save timestamp
+WEB_ROOT=/var/www/html/OsmHiCheck
+
+#database credentials for osmosis
+AUTH_FILE=$HOME/.osmosis.auth
+#--------------------------------------------------------
+
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+OSMOSIS=osmosis
+OSMFILTER=osmfilter
+OSMUPDATE=osmupdate
+OSMCONVERT=osmconvert
 
 #filenames
 PRESENT_DUMP=converted_dump.o5m
@@ -23,9 +30,6 @@ PRESENT_FILTERED_DUMP=filtered_dump.o5m
 NEXT_DUMP=updated_converted_dump.o5m
 NEXT_FILTERED_DUMP=updated_filtered_dump.o5m
 UPDATE=update_database.osc
-
-#database credential
-AUTH_FILE=$HOME/.osmosis.auth
 
 LOCK=/tmp/xsvana00.lock
 test -f $LOCK && { echo "Lock file $LOCK exists. Exiting..." >>$LOGFILE; exit; }
@@ -42,12 +46,13 @@ echo ------------------------------------------------------------ >> $LOGFILE
 CURRENT=`pwd`
 cd $WORK_DIR
 
-$OSMUPDATE $PRESENT_DUMP $NEXT_DUMP
+#clip by CZ polygon - got it on http://download.geofabrik.de/europe/czech-republic.html
+$OSMUPDATE $PRESENT_DUMP $NEXT_DUMP -B=czech-republic.poly
 ret=$?
 echo `date` >> $LOGFILE
 if [ "$ret" = "21" ]; then 	
 	echo NOTICE: Your OSM file is already up-to-date. Aborting...  >> $LOGFILE
-	echo `date '+%d.%m.%Y %H:%M'` > $LASTUPDATE
+	echo `date '+%d.%m.%Y %H:%M'` > $WEB_ROOT/last_update.txt
 	exit 0
 elif [ "$ret" != "0" ]; then
 	echo ERROR: Unsuccesful download of updated dump. [$ret] >> $LOGFILE
@@ -81,15 +86,15 @@ if [ "$ret" != "0" ]; then
 	exit 1
 else
 	echo "Database updated. [$ret]" >> $LOGFILE
-	echo `date '+%d.%m.%Y %H:%M'` > $LASTUPDATE
-	php /var/www/html/xsvana00/tables/php/saveStats.php
+	echo `date '+%d.%m.%Y %H:%M'` > $WEB_ROOT/last_update.txt
+	php $WEB_ROOT/tables/php/saveStats.php
 fi
 
-#remove stuff outside of region of interest - CZ
-$OSMCONVERT -B=czech-republic.poly $NEXT_DUMP >$PRESENT_DUMP
-rm $NEXT_DUMP
-
+#remove old unused stuff
+mv $NEXT_DUMP $PRESENT_DUMP
 mv $NEXT_FILTERED_DUMP $PRESENT_FILTERED_DUMP
+rm $UPDATE
+
 cd $CURRENT
 
 exit 0
