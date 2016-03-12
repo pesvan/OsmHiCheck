c02109c4467a544b77f278b179a208c4cd0dfe33
tkas <tomas.kasparek@gmail.com>
2016-03-12 21:53:10 +0100

    split status graph into two images - different Y ranges

--- gp/stats.php
+++ gp/stats.php
@@ -12,12 +12,23 @@ function getStatsForGraphs(){ //{{{
     $cnt = 0;
     while($row=pg_fetch_assoc($result)){
         $ret[$cnt][0]=$row['date'];
-        $ret[$cnt][1]=intval($row['node_total']);
         $ret[$cnt][2]=intval($row['node_ok']);
         $ret[$cnt][3]=intval($row['node_bad']);
         $ret[$cnt][4]=intval($row['node_cor']);
-        $ret[$cnt][5]=intval($row['img_total']);
         $ret[$cnt][6]=intval($row['img_used']);
+        $cnt++;
+    }
+    return $ret;
+} //}}}
+
+function getStatsForGraphsTot(){ //{{{
+    $result = pg_query("SELECT date, node_total, node_ok, node_bad, node_cor, img_total, img_used FROM hicheck.gp_stats ORDER BY date DESC LIMIT ".DAYS_MAX);
+    $ret = array();
+    $cnt = 0;
+    while($row=pg_fetch_assoc($result)){
+        $ret[$cnt][0]=$row['date'];
+        $ret[$cnt][1]=intval($row['node_total']);
+        $ret[$cnt][5]=intval($row['img_total']);
         $cnt++;
     }
     return $ret;
@@ -37,13 +48,27 @@ function prepareGraphs($data){ //{{{
     return array_reverse($data);
 } //}}}
 
-$plot = new PHPlot(1200, 500);
+  $plot = new PHPlot(1200, 600);
 
-$data = getStatsForGraphs();
-$data = prepareGraphs($data);
-$plot->SetDataValues($data);
+if(isset($_GET['tot'])){
+  $data = getStatsForGraphsTot();
+  $data = prepareGraphs($data);
+  $plot->SetDataValues($data);
+  $plot->SetLegend(array("nodes total", "photos total"));
+  $plot->SetLegendPosition(0,0.0, 'plot', 0.05,0.05);
+  $plot->DrawGraph();
+}
+if(isset($_GET['mis'])){
+  $data = getStatsForGraphs();
+  $data = prepareGraphs($data);
+  $plot->SetDataValues($data);
+  $plot->SetLegend(array("node OK", "node miss", "node check", "photos used"));
+  $plot->SetLegendPosition(0,-0.2, 'plot', 0.05,0.05);
+  $plot->DrawGraph();
+}
+?>
 
-$plot->SetLegend(array("nodes total", "node OK", "node miss", "node check", "photos total", "photos used"));
-$plot->SetLegendPosition(0.0,0.0, 'image', 0.05,0.05);
-
-$plot->DrawGraph();
+<html>
+<p><img src="stats.php?tot"/></p>
+<p><img src="stats.php?mis"/></p>
+</html>
