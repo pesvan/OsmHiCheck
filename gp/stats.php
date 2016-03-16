2f555a7f0762dd4975d1e96c4033fb328318868a
tkas <tomas.kasparek@gmail.com>
2016-03-16 16:50:00 +0100

    rearange graph layout

--- gp/stats.php
+++ gp/stats.php
@@ -7,15 +7,14 @@ require_once dirname(__FILE__).'/../tables/php/phplot/phplot.php';
 define('DAYS_MAX', 42); #6 weeks
 
 function getStatsForGraphs(){ //{{{
-    $result = pg_query("SELECT date, node_total, node_ok, node_bad, node_cor, img_total, img_used FROM hicheck.gp_stats ORDER BY date DESC LIMIT ".DAYS_MAX);
+    $result = pg_query("SELECT date, node_total, node_bad, node_cor, img_total, img_total - img_used AS img_check FROM hicheck.gp_stats ORDER BY date DESC LIMIT ".DAYS_MAX);
     $ret = array();
     $cnt = 0;
     while($row=pg_fetch_assoc($result)){
         $ret[$cnt][0]=$row['date'];
-        $ret[$cnt][2]=intval($row['node_ok']);
-        $ret[$cnt][3]=intval($row['node_bad']);
-        $ret[$cnt][4]=intval($row['node_cor']);
-        $ret[$cnt][6]=intval($row['img_used']);
+        $ret[$cnt][1]=intval($row['node_bad']);
+        $ret[$cnt][2]=intval($row['node_cor']);
+        $ret[$cnt][3]=intval($row['img_check']);
         $cnt++;
     }
     return $ret;
@@ -27,8 +26,9 @@ function getStatsForGraphsTot(){ //{{{
     $cnt = 0;
     while($row=pg_fetch_assoc($result)){
         $ret[$cnt][0]=$row['date'];
-        $ret[$cnt][1]=intval($row['node_total']);
-        $ret[$cnt][5]=intval($row['img_total']);
+        $ret[$cnt][1]=intval($row['node_ok']);
+        $ret[$cnt][2]=intval($row['node_total']);
+        $ret[$cnt][3]=intval($row['img_total']);
         $cnt++;
     }
     return $ret;
@@ -54,7 +54,7 @@ if(isset($_GET['tot'])){
   $data = getStatsForGraphsTot();
   $data = prepareGraphs($data);
   $plot->SetDataValues($data);
-  $plot->SetLegend(array("nodes total", "photos total"));
+  $plot->SetLegend(array("nodes OK", "nodes total", "photos total"));
   $plot->SetLegendPosition(0,0.0, 'plot', 0.05,0.05);
   $plot->DrawGraph();
 }
@@ -62,7 +62,7 @@ if(isset($_GET['mis'])){
   $data = getStatsForGraphs();
   $data = prepareGraphs($data);
   $plot->SetDataValues($data);
-  $plot->SetLegend(array("node OK", "node miss", "node check", "photos used"));
+  $plot->SetLegend(array("nodes miss", "nodes check", "photos check"));
   $plot->SetLegendPosition(0,-0.2, 'plot', 0.05,0.05);
   $plot->DrawGraph();
 }
